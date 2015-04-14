63594a13dba83fa6cabe5ef309213366c5b6ccd0
Tobias Fritz <tfr@itscope.de>
2015-04-14 19:12:35 +0200

    configured Maven install

---
+++ src-test/ditem/item/PropertyListTestJ.java
@@ -0,0 +1,13 @@
+package ditem.item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PropertyListTestJ {
+	public PropertyListTestJ() {
+		List<String> names = new ArrayList<String>();
+		String[] namesAr = new String[4];
+		PropertyList<String> list = new PropertyList<String>(names);
+				
+	}
+}
